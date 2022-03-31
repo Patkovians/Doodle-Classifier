18db859d6d7cc42247f0ff94e62db44296b0b5c0
Una Adilović <adilovicuna@gmail.com>
2022-04-01 01:22:41 +0200

    update

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from dataset.Dataset import Dataset
 from PIL import Image
 
 if __name__ == "__main__":
-    # with open("dataset/features.txt") as file:
-    #     a = None
-    #     for line in file:
-    #         a = Dataset(name=line.strip())
-
-    a = Dataset("panda")
-    print(a.data.shape)
-
-    img = Image.fromarray(a.data[4].reshape(28, 28))
-    img.save("img.png")
+    with open("dataset/features.txt") as file:
+        a = None
+        for line in file:
+            a = Dataset(name=line.strip())
